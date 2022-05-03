991237bd934ebeb738457ddbe65926d6766c266a
Oreilleu <aurelien.picardpabijan@gmail.com>
2022-05-03 19:04:12 +0200

    route like

--- back/controllers/post.controller.js
+++ back/controllers/post.controller.js
@@ -3,78 +3,124 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 module.exports.createPost = (req, res) => {
-    const { id } = req.params
-    const { userId, message, picture, video } = req.body
-    // const data = { userId, message, picture, video }
-    prisma.Post.create({
-        // Comment passer dinamiquement le userId ??
-        data: {
-            userId: parseInt(userId),
-            message: message,
-            picture,
-            video
-        }
+  const { id } = req.params;
+  const { userId, message, picture, video } = req.body;
+  // const data = { userId, message, picture, video }
+  prisma.Post.create({
+    // Comment passer dinamiquement le userId ??
+    data: {
+      userId: parseInt(userId),
+      message: message,
+      picture,
+      video,
+    },
+  })
+    .then(() => {
+      res.status(201).json({ message: 'post créer' });
     })
-        .then(() => { res.status(201).json({ message: "post créer" }) })
-        .catch(err => res.status(400).json({ err }))
-}
+    .catch((err) => res.status(400).json({ err }));
+};
 
 module.exports.getAllPost = (req, res) => {
-    //   console.log(req.body);
-    prisma.Post.findMany({})
-        .then((post) => res.status(200).json({ post }))
-        .catch((err) => res.status(400).json({ err }));
-
-    //   try {
-    //     const products = await prisma.User.findMany({});
-    //     res.json(products);
-    //   } catch (error) {
-    //     console.log(error);
-    //     res.send('no');
-    //   }
+  //   console.log(req.body);
+  prisma.Post.findMany({})
+    .then((post) => res.status(200).json({ post }))
+    .catch((err) => res.status(400).json({ err }));
 };
 
 module.exports.getOnePost = (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    prisma.Post.findUnique({
-        where: { id: parseInt(id) },
-    })
-        .then((post) =>
-            post
-                ? res.status(200).json({ post })
-                : res.status(404).json({ message: `Le post n'existe pas ${id}` })
-        )
-        .catch((err) => res.status(500).json({ err }));
+  prisma.Post.findUnique({
+    where: { id: parseInt(id) },
+  })
+    .then((post) =>
+      post
+        ? res.status(200).json({ post })
+        : res.status(404).json({ message: `Le post n'existe pas ${id}` })
+    )
+    .catch((err) => res.status(500).json({ err }));
 };
 
 module.exports.updatePost = (req, res) => {
-    const { id } = req.params;
-    const { message, picture, video } = req.body;
+  const { id } = req.params;
+  const { message, picture, video } = req.body;
 
-    prisma.Post.update({
-        where: {
-            id: parseInt(id),
-        },
-        data: {
-            message,
-            picture,
-            video
-        },
-    })
-        .then(() => res.status(201).json({ message: 'Post modifer' }))
-        .catch((err) => res.status(400).json({ err }));
+  prisma.Post.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: {
+      message,
+      picture,
+      video,
+    },
+  })
+    .then(() => res.status(201).json({ message: 'Post modifer' }))
+    .catch((err) => res.status(400).json({ err }));
 };
 
 module.exports.deletePost = (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    prisma.Post.delete({
-        where: {
-            id: parseInt(id),
-        },
-    })
-        .then(() => res.status(201).json({ message: 'Post supprimer' }))
-        .catch((err) => res.status(400).json({ err }));
+  prisma.Post.delete({
+    where: {
+      id: parseInt(id),
+    },
+  })
+    .then(() => res.status(201).json({ message: 'Post supprimer' }))
+    .catch((err) => res.status(400).json({ err }));
 };
 
+module.exports.likePost = (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.body;
+
+  //   prisma.Like.create({
+  //     data: {
+  //       userId: parseInt(userId),
+  //       postId: parseInt(id),
+  //     },
+  //   })
+  //     .then(() => {
+  //       prisma.Like.findUnique({
+  //         where: {
+  //           userId: parseInt(userId),
+  //         },
+  //       })
+  //         .then(() => res.send({ message: 'déja like' }))
+  //         .catch((err) => res.send({ err }));
+  //     })
+  //     .catch(() => res.send('ko'));
+
+  //   prisma.like
+  //     .findUnique({
+  //       where: {
+  //         postId: parseInt(id),
+  //       },
+  //     })
+  //     .then(() => {
+  //       userId ? res.send('oui') : res.send('non');
+  //     })
+  //     .catch((err) => res.send({ err }));
+
+  prisma.Like.findUnique({
+    where: {
+      userId: parseInt(userId),
+    },
+  })
+    .then((postLike) => {
+      console.log(postLike);
+      // postLike
+      //   ? res.send({ message: 'déja liké' })
+      //   : prisma.Like.create({
+      //       data: {
+      //         userId: parseInt(userId),
+      //         postId: parseInt(id),
+      //       },
+      //     });
+    })
+    .catch((err) => res.send({ err }));
+};
+
+module.exports.unlikePost = (req, res) => {};
--- back/routes/post.routes.js
+++ back/routes/post.routes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
-const postCtrl = require('../controllers/post.controller')
+const postCtrl = require('../controllers/post.controller');
 
+// CRUD post
+router.post('/', postCtrl.createPost);
+router.get('/', postCtrl.getAllPost);
+router.get('/:id', postCtrl.getOnePost);
+router.put('/:id', postCtrl.updatePost);
+router.delete('/:id', postCtrl.deletePost);
 
-router.post('/', postCtrl.createPost)
-router.get('/', postCtrl.getAllPost)
-router.get('/:id', postCtrl.getOnePost)
-router.put('/:id', postCtrl.updatePost)
-router.delete('/:id', postCtrl.deletePost)
+// Likes
+router.patch('/like-post/:id', postCtrl.likePost);
+router.patch('/unlike-post/:id', postCtrl.unlikePost);
+
+// Comments
 
 module.exports = router;
