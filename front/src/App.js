a478de316f305085ab33fd8377bc3778b275e5f7
Oreilleu <aurelien.picardpabijan@gmail.com>
2022-05-10 20:02:16 +0200

    ajout frontend

---
+++ front/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Router from './component/Routes';
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <Router />
+    </React.Fragment>
+  );
+}
---
+++ front/src/component/Routes/index.js
@@ -0,0 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  //   Redirect,
+} from 'react-router-dom';
+import Home from '../../page/Home';
+import Profil from '../../page/Profil';
+import Trending from '../../page/Trending';
+
+export default function index() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/profil" exact component={Profil}></Route>
+        <Route path="/trending" exact component={Trending}></Route>
+        {/* <Redirect to="/" /> */}
+      </Switch>
+    </Router>
+  );
+}
